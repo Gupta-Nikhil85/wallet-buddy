cf8d1111f00f246875e45e6fe9a16a7a2b664d1e
sahil duhan <duhan.sahil16@gmail.com>
2022-03-13 03:16:13 +0530

    login added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Hero from './components/Hero';
+import Login from './components/Login';
 import Main from './components/Main';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <>
-    <Hero/>
-    <Main/>
+      <Navbar/>
+      <Login/>
+      <Hero/>
+      <Main/>
     </>
   );
 }
